5c5cc35ac2ed7cda8304c884d7ce9274e409487a
Long3112 <128303674+Long3112@users.noreply.github.com>
2024-01-29 19:13:34 +0700

    Update FileS.java
    ok

--- src/file/FileS.java
+++ src/file/FileS.java
@@ -1,5 +1,6 @@
 package file;
 
+import manager.StudentManager;
 import model.Student;
 
 import java.io.*;
@@ -7,25 +8,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileS {
-    static File file = new File("students.txt");
-
-    public static void write(List<Student> students) {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(file);
-             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
-            objectOutputStream.writeObject(students);
-        } catch (IOException e) {
+    static File studentFile = new File("student.csv");
+    public static void writeStudent(List<Student> students) {
+        try {
+            FileWriter fileWriter = new FileWriter(studentFile);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for (Student s : students) {
+                bufferedWriter.write(s.toString());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    public static List<Student> readStudent() {
+        ArrayList<Student> students = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(studentFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String str = "";
+            while ((str = bufferedReader.readLine()) != null) {
+                String[] arr = str.split(",");
+                Student student = new Student(Integer.parseInt(arr[0]),arr[1], Double.parseDouble(arr[2]), Double.parseDouble(arr[3]),Double.parseDouble(arr[4]));
+                students.add(student);
+            }
 
-    public static List<Student> read() {
-        try (FileInputStream fileInputStream = new FileInputStream(file);
-             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
-            return (List<Student>) objectInputStream.readObject();
+
+
         } catch (Exception e) {
             e.printStackTrace();
-            return new ArrayList<>();
         }
+        return students;
     }
 
 }
